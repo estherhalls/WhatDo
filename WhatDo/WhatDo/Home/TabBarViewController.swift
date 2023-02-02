e22f59db9d744221bdc05f9586c1093958ec04ae
Esther Halls <esthernhalls@gmail.com>
2023-02-02 12:20:16 -0700

    added translucent white background to tab bar so it has some separation on the map page, but still looks good against the swipe page gradient background

--- WhatDo/WhatDo/Home/TabBarViewController.swift
+++ WhatDo/WhatDo/Home/TabBarViewController.swift
@@ -7,23 +7,28 @@
 
 import UIKit
 
-class TabBarViewController: UITabBarController {
+class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.tabBar.backgroundColor = UIColor(white: 1.0, alpha: 0.65
+)//        self.viewControllers = [firstTab(), secondTab(), thirdTab(), fourthTab(), fifthTab()]
+        self.delegate = self
     }
-    
+//    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+//        // If view controller is UIViewController as? YelpCVViewController and global location paramaters are not set, return false and trigger alert controller -> set location -> trigger this function again? or another way to navigate forward
+//        if tabBarController.selectedIndex > 0 && longitude == nil && latitude == nil {
+//            return false
+//        }
+//        return true
+//    }
 
-    /*
-    // MARK: - Navigation
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        // If view controller is UIViewController as? YelpCVViewController (or any index greater than home/0) and global location paramaters are not set, return false and trigger alert controller -> set location -> trigger this function again? or another way to navigate forward
+//        self.navigationController?.pushViewController(demoThreeVC, animated: true) but the VC name is entered using dependency injection based on the index number being selected?
+        print("Tab Index:", tabBarController.selectedIndex)
+//        if tabBarController.selectedIndex > 0
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
